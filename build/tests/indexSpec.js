904cd82d1dd869704679860b29f45d1c422cbef6
Ahmed Orabi <aorabi@outlook.com>
2022-05-05 19:37:12 +0300

    remove build folder

--- build/tests/indexSpec.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../index");
-it('result expected to be num * 5 ', () => {
-    expect((0, index_1.default)(10)).toEqual(50);
-});
